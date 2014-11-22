c715e04350ed008d68ffed89d358773c7cfa5b29
Thomas Howard <thomashoward206@gmail.com>
2014-11-22 17:08:42 -0500

    Commit 3
    Basic use of Css Selectors

---
+++ CssSelector.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/*
+public class Gmail_CssSelector {
+
+	public static void main(String[] args) {
+		
+		FirefoxDriver wd = new FirefoxDriver();
+		wd.get("http://www.gmail.com");
+		wd.findElement(By.cssSelector("input[id='Email']")).sendKeys("thomashoward206@gmail.com");;
+		wd.findElement(By.cssSelector("input[id='Passwd']")).sendKeys("demarist");;
+
+		
+	}
+
+}
+*/
