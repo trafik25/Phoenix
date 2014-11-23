1dcdeb6b57d7b6bb2cb789cf33a8bd5e749c5c6a
Thomas Howard <thomashoward206@gmail.com>
2014-11-23 17:29:55 -0500

    Commit 8
    Desired Capabilities in Selenium

--- Advanced Selenium/DesiredCapabilities.java
+++ Advanced Selenium/DesiredCapabilities.java
@@ -1,1 +1,18 @@
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
 public class DesiredCap {
+
+	public static void main(String[] args) {
+
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		capabilities.setJavascriptEnabled(true);
+		//capabilities.setCapability(CapabilityType.Proxy, value);
+		
+		FirefoxDriver wd = new FirefoxDriver(capabilities); 
+		
+	}
+
+}
