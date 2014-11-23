12cca2d70ab72d11496d7bcc86d124deb311c875
Thomas Howard <thomashoward206@gmail.com>
2014-11-23 17:29:31 -0500

    Commit 7
    Java Basics and selenium scripts

---
+++ Java Basics/Concatanation.java
@@ -0,0 +1,25 @@
+
+public class Concatanation_Operator {
+
+	
+	public static void main(String[] args) {
+
+		
+	String x = "Hello";
+	String y = "World";
+	
+	int a =100;
+	int b =200;
+	
+	
+	System.out.println(x+y);  //+ is concatenating operator
+	System.out.println(a+b);
+	System.out.println(x+y);
+	
+	
+	
+	}
+	
+	
+	
+}
