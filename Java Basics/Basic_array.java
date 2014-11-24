8b58933897e42ee10025c0a8780c4ee13cd2689f
Thomas Howard <thomashoward206@gmail.com>
2014-11-24 14:23:27 -0500

    Commit 11
    A Basic array for reading data in Selenium

---
+++ Java Basics/Basic_array.java
@@ -0,0 +1,56 @@
+
+public class Arrays {
+
+	public static void main(String[] args) {
+
+		String username = "1111";
+		String password = "1221";
+		String email	= "asd@gmail.com";
+		String zip = "12342";
+		String city = "London";
+		
+		//Entering a large amount of data into a web page. 
+		
+		String str[] = new String[5];
+		str[0]= "1111";
+		str[1]= "1221";
+		str[2]= "asd@gmail.com";
+		str[3]= "12342";
+		str[4]= "London";
+		
+		//print all values in the array
+		//for(int x=0; x<str.length; x++  ) {
+		//System.out.println(x);
+		
+		//different types of arrays
+		int i[] = new int[4];
+		i[0]= 232;
+		i[1]= 343;
+		i[2]= 43;
+		i[3]= 1212;
+		
+		for(int y=0; y<i.length; y++  ) {
+			System.out.println(i[y]);
+		
+		}
+	
+		
+		char c[] = new char[44];
+		
+		
+		//printing arrays
+		/*
+		System.out.println(str.length);
+		System.out.println(i.length);
+		System.out.println(c.length);
+		*/
+
+		
+		}
+		
+		
+		
+		
+	}
+
+
