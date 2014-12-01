27e5d19725e91fda9bd92ba2b37a67b6c5f68839
Thomas Howard <thomashoward206@gmail.com>
2014-11-30 23:14:35 -0500

    Commit 1: 11/30
    Basic for loop in java

---
+++ Java Basics/forloop.java
@@ -0,0 +1,23 @@
+
+
+
+
+public class For_Loop {
+	
+	{
+
+	for(int x=0; x<10; x++  ) {
+		System.out.println(x);
+		
+	}
+	
+	int sum= 0;
+	for(int n=0;n<=100;n++ ) {
+		sum=sum+n;
+	}
+	System.out.println("The sum of the first 100 numbers is : " +sum);
+	
+	
+}
+	
+}
