f003b33000f91defa444e0e0435bd8d6162c7285
Thomas Howard <thomashoward206@gmail.com>
2014-12-05 08:36:23 -0500

    Update WebTables_TotalColumns.java
    Updated name in file

--- WebTables/WebTables_TotalColumns.java
+++ WebTables/WebTables_TotalColumns.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class WebTables {
+public class WebTables_TotalColumns {
 
 	public static void main(String[] args) {
 
