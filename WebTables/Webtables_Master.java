5f4a965406fbb3343be98e77910de0a4357fba43
Thomas Howard <thomashoward206@gmail.com>
2014-12-05 08:35:55 -0500

    Update Webtables_Master.java
    Updated name in file

--- WebTables/Webtables_Master.java
+++ WebTables/Webtables_Master.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class WebTables {
+public class WebTables_Master {
 
 	public static void main(String[] args) {
 
