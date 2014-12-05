bafba3bcd7fc61ed651f7478b714038d392d56fe
Thomas Howard <thomashoward206@gmail.com>
2014-12-05 08:37:40 -0500

    Update WebTables_CompleteValuesinTable.java
    Updated name in file

--- WebTables/WebTables_CompleteValuesinTable.java
+++ WebTables/WebTables_CompleteValuesinTable.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Complete_List {
+public class WebTables_CompleteValuesinTable {
 
 	public static void main(String[] args) {
 
